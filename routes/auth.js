8fba1dfc97b2bd9c77baec545329e4af35489758
JavierRDA01 <javier.ramirezdeandres34439@gmail.com>
2025-09-28 13:20:27 +0200

    TODO: register en el router

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,18 @@
 
 const express = require('express');
 const router = express.Router();
+const userQueries = require('../data/users');
+
 
 router.get('/register', (req, res)=>{
 
+    userQueries.obtenerConcesionarios((err, concesionarios)=>{
+        if (err) {
+            console.error("Error al obtener los concesionarios:", err);
+            return res.status(500).render("error", { mensaje: "Error al obtener los concesionarios." });
+        }
+
+    })
 
     
 
